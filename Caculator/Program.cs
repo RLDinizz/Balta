c0c2be93ced5c58ce5641125312aa1a22fc4ed76
RLDinizz <83675304+RLDinizz@users.noreply.github.com>
2022-10-22 14:48:17 -0300

    Criando Calculadora

---
+++ Caculator/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Object que deseja fazer");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("9 - Sair");
+
+            Console.Write("Selecione uma opção: ");
+            var res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Somar(); break;
+                case 2: Subtrair(); break;
+                case 3: Dividir(); break;
+                case 4: Multiplicar(); break;
+                case 9: System.Environment.Exit(0); break;
+            }
+            Menu();
+        }
+        static void Somar()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Somar 2 valores.....");
+            Console.WriteLine("Primeiro valor:");
+            var vlr1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            var vlr2 = float.Parse(Console.ReadLine());
+
+            var resultado = vlr1 + vlr2;
+
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+            Console.ReadKey();
+        }
+        static void Subtrair()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Subtrair 2 valores.....");
+            Console.WriteLine("Primeiro valor:");
+            var vlr1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            var vlr2 = float.Parse(Console.ReadLine());
+
+            var resultado = vlr1 - vlr2;
+
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey();
+        }
+        static void Dividir()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Dividir 2 valores.....");
+            Console.WriteLine("Primeiro valor:");
+            var vlr1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            var vlr2 = float.Parse(Console.ReadLine());
+
+            var resultado = vlr1 / vlr2;
+
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
+            Console.ReadKey();
+        }
+        static void Multiplicar()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Multiplicar 2 valores.....");
+            Console.WriteLine("Primeiro valor:");
+            var vlr1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            var vlr2 = float.Parse(Console.ReadLine());
+
+            var resultado = vlr1 * vlr2;
+
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+            Console.ReadKey();
+        }
+
+    }
+}
+
--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -4,9 +4,50 @@ namespace MeuApp
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var mouse = new Product(1235446, "Gamer", 359.90, EProductType.Product);
+            var manutencaoPC = new Product(1235447, "Formatar PC", 50.90, EProductType.Service);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+
+
+            // Console.WriteLine(mouse.PriceInDolar(5.12));
+
+
         }
     }
+
+    struct Product
+    {
+        public Product(int id, string name, double price, EProductType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Type = type;
+
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+        public EProductType Type;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2,
+    }
+
+
 }
